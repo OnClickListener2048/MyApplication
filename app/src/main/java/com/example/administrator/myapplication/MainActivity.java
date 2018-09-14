71c4e8e3089fcf9f075be1f26fec1cc4e541703d
wangxin <55997027@qq.com>
2018-09-14 16:54:22 +0800

    third commit

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //first commit
         //second commit
+        //third commit
     }
 }
